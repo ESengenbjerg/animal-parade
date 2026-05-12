d9e180265099e19bcff4b37b5a35af2843bb026d
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-05-12 09:51:07 +0200

    Added script to trigger API request, redirect to Tivoli if error occurs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,48 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { startTransaction } from "@/lib/api/centralbank";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [token, setToken] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  // Read token from URL
+  useEffect(() => {
+    const t = searchParams.get("identity_token");
+    if (t) {
+      setToken(t);
+      // Remove token from URL
+      window.history.replaceState(null, "", "/");
+    }
+  }, [searchParams]);
+
+  async function handleStart() {
+    if (!token) return;
+
+    try {
+      setLoading(true);
+
+      const result = await startTransaction(
+        token, //Token from URL
+        2, // Entrance fee
+        process.env.NEXT_PUBLIC_AMUSEMENT_UUID!, // Amusement's Api key
+      );
+
+      // Redirect to attraction page
+      router.push(
+        `/animal?stamp=${encodeURIComponent(JSON.stringify(result.stamp))}`,
+      );
+    } catch (err) {
+      console.error(err);
+      alert("Your token has expired. Please return to the Tivoli.");
+      router.push("https://tivoli-develop.up.railway.app/");
+    }
+  }
+
   return (
     <main
       className="h-screen bg-cover bg-center"
@@ -17,18 +59,18 @@ export default function Home() {
           </p>
         </article>
         <article className="pt-48">
-          <form action="/api/transactions" method="POST">
-            {/* Required body fields for the API are sent as hidden inputs */}
-            <input type="hidden" name="identity_token" value="" />
-            <input type="hidden" name="amount" value="2" />
-            <input type="hidden" name="amusement_uuid" value="" />
-            <button
-              type="submit"
-              className="px-8 py-4 text-2xl font-semibold rounded-xl shadow-lg bg-orange-400 hover:bg-orange-500 text-white transition"
-            >
-              See an animal
-            </button>
-          </form>
+          <button
+            onClick={handleStart}
+            disabled={!token || loading}
+            className="px-8 py-4 text-2xl font-semibold rounded-xl shadow-lg bg-orange-400 hover:bg-orange-500 text-white transition"
+          >
+            {loading ? "Loading..." : "See an animal"}
+          </button>
+          {!token && (
+            <p className="text-sm text-red-700 mt-4">
+              <strong>ERROR! </strong> You must enter through the Tivoli to play
+            </p>
+          )}
         </article>
       </section>
     </main>
