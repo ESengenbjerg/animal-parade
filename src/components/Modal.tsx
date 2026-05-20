c97adb204b467f7fffa729d7e1a9a8d7380b93c2
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-05-20 13:22:47 +0200

    Added styling to modals

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,14 +14,14 @@ export default function Modal({
   if (!open) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-9999">
-      <div className="bg-white rounded-xl shadow-xl p-8 max-w-md text-center text-black">
+    <div className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-9999">
+      <div className="flex flex-col justify-center items-center bg-white rounded-xl shadow-xl p-8 text-center text-black w-100 h-80 max-w-[90%]">
         <h2 className="text-2xl font-bold mb-4">{title}</h2>
         <p className="mb-6">{message}</p>
 
         <button
           onClick={onClose}
-          className="px-6 py-3 bg-orange-400 hover:bg-orange-500 text-white rounded-lg font-semibold"
+          className="px-6 py-3 bg-orange-400 hover:bg-orange-500 text-white rounded-lg font-semibold w-40"
         >
           OK
         </button>
