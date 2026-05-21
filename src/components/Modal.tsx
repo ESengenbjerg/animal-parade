6f6f37ce691b8fc0c04759a602c09d29caad326b
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-05-21 22:04:35 +0200

    Updated background for modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,7 +14,7 @@ export default function Modal({
   if (!open) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-9999">
+    <div className="fixed inset-0 bg-neutral-900/40 backdrop-blur-sm flex items-center justify-center z-50">
       <div className="flex flex-col justify-center items-center bg-white rounded-xl shadow-xl p-8 text-center text-black w-100 h-80 max-w-[90%]">
         <h2 className="text-2xl font-bold mb-4">{title}</h2>
         <p className="mb-6">{message}</p>
