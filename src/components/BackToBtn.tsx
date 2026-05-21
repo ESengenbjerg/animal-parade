a8790bfeec6b530fbb74db693203316c9c36c397
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-05-21 21:27:40 +0200

    Preparation for activating <iframe> when it goes into production

--- src/components/BackToBtn.tsx
+++ src/components/BackToBtn.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 export default function BackToBtn() {
   return (
     <article className="mt-12">
+      {/* Navigate with tabs button */}
       <Link
         href="https://frontend-main-1ac7.up.railway.app/user"
         aria-label="Go back to Tivoli"
@@ -17,6 +18,16 @@ export default function BackToBtn() {
           Back to Tivoli
         </button>
       </Link>
+
+      {/* Preparation for <iframe> button */}
+      {/* <button
+        onClick={() =>
+          window.parent.postMessage({ type: "AMUSEMENT_CLOSE" }, "*")
+        }
+        className="px-8 py-4 text-2xl font-semibold rounded-xl shadow-lg bg-orange-400 hover:bg-orange-500 text-white transition"
+      >
+        Back to Loopland
+      </button> */}
     </article>
   );
 }
