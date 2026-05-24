c0b8af2300eec96357b3c0c647f2e4c383a04b74
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-05-24 12:38:08 +0200

    Changed background behind modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,7 +14,7 @@ export default function Modal({
   if (!open) return null;
 
   return (
-    <div className="fixed inset-0 bg-neutral-900/40 backdrop-blur-sm flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-gray-500/30 backdrop-blur-md flex items-center justify-center z-50">
       <div
         className="flex flex-col justify-center items-center
       bg-white text-black rounded-xl shadow-xl text-center p-4 m-8 w-full max-w-[90%]
@@ -24,12 +24,6 @@ export default function Modal({
         <h2 className="text-xl font-bold mb-4 md:text-2xl">{title}</h2>
         <p className="mb-6 text-sm md:text-base">{message}</p>
 
-        {/* <button
-          onClick={onClose}
-          className="px-6 py-3 bg-orange-400 hover:bg-orange-500 text-white rounded-lg font-semibold w-40"
-        >
-          OK
-        </button> */}
         <button
           onClick={() =>
             window.parent.postMessage({ type: "AMUSEMENT_CLOSE" }, "*")
