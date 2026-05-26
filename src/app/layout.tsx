7881f5f7017e37a566cfc9c72739ea1c3c108879
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-05-26 19:14:45 +0200

    Added Google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Fredoka } from "next/font/google";
+import { Nunito } from "next/font/google";
+
+export const fredoka = Fredoka({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-fredoka",
+});
+
+export const nunito = Nunito({
+  subsets: ["latin"],
+  variable: "--font-nunito",
+});
 
 export const metadata: Metadata = {
   title: "Animal Parade",
@@ -12,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${nunito.variable} ${fredoka.variable}`}>
       <body>
         <div id="page-transition" className="page-transition"></div>
         {children}
