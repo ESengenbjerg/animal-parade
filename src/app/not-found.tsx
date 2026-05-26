47b321cff6d86924b1681f4f23c7582f2622e9d3
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-05-26 15:52:42 +0200

    Added styling for not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,21 @@
-import BackToBtn from "@/components/BackToBtn";
+import Background from "@/components/Background";
 
 export default function NotFound() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-4xl font-bold mb-4">404 — Lost in the Parade</h1>
-      <p className="mb-6">This animal seems to have wandered off...</p>
-      <BackToBtn />
-    </main>
+    <Background>
+      <main className="flex flex-col items-center justify-center h-screen text-center">
+        <section className="flex flex-col justify-center items-center w-1/3 text-center">
+          <h1 className="text-4xl font-bold mb-4">404 — Lost in the Parade</h1>
+          <p className="mb-6">This animal seems to have wandered off...</p>
+          {/* Custom button for browser, user on Loopland shouldn't be able to get here */}
+          <a
+            href="/"
+            className="bg-orange-400 hover:bg-orange-500 text-white text-xl px-6 py-3 rounded-xl shadow-lg transition md:text-2xl"
+          >
+            Back to start
+          </a>
+        </section>
+      </main>
+    </Background>
   );
 }
